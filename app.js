83d6abf83d496ef9d7aae856377e0a9e9d9d34db
BAHOHCEHC <bahohcehc@gmail.com>
2020-06-05 15:54:47 +0300

    keys

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
 
-const authRoutes = require("./routes/auth");
-const clientsRoutes = require("./routes/clients");
-const taskRoutes = require("./routes/task");
-const statisticRoutes = require("./routes/statistic");
+const authRoutes = require('./routes/auth');
+const clientsRoutes = require('./routes/clients');
+const taskRoutes = require('./routes/task');
+const statisticRoutes = require('./routes/statistic');
 
-const keys = require("./config/keys");
+const keys = require('./config/keys');
 const app = express();
 
 mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  })
-  .then(() => console.log("MongoDB connected."))
-  .catch(error => console.log(error));
-	
+	.connect(keys.mongoURI, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => console.log('MongoDB connected.'))
+	.catch((error) => console.log(error));
+
 // использование стратегий доступа
 app.use(passport.initialize());
-require("./middleware/passport")(passport);
+require('./middleware/passport')(passport);
 
-app.use(morgan("dev"));
-app.use("/uploads", express.static("uploads"));
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: true })); // парсит данные из запроса
 app.use(bodyParser.json()); //генериит js объекты из json-a
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/clients", clientsRoutes);
-app.use("/api/statistic", statisticRoutes);
-app.use("/api/task", taskRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/clients', clientsRoutes);
+app.use('/api/statistic', statisticRoutes);
+app.use('/api/task', taskRoutes);
+
+
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/dist/client'));
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
+		);
+	});
+}
 
 module.exports = app;
